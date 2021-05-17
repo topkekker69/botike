b5b782608d4cf3155ee93ac1400ad8ca56434eaa
topkekker69 <83924950+topkekker69@users.noreply.github.com>
2021-05-17 23:11:53 +0300

    Add files via upload

---
+++ shopItems.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        item: 'Sword',
+        price: 100
+    },
+    {
+        item: 'Fruits',
+        price: 1000
+    },
+    {
+        item: "Sivler",
+        price: 15000
+    },
+    {
+        item: 'Gold',
+        price: 50000
+    },
+    {
+        item: 'Diamond',
+        price: 100000
+    },
+    {
+        item: 'Mom',
+        price: 150000
+    },
+    {
+        item: 'Pocketpussy',
+        price: 250000
+    },
+    {
+        item: 'Dildo',
+        price: 500000
+    },
+    {
+        item: 'CustomRole',
+        price: 1000000
+    }
+    
+]
